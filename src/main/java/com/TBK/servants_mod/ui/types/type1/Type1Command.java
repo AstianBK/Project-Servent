daefdcafb362813b65940cad63d312c6b1d96f77
SangreBK <SangreBK@gmail.com>
2026-04-26 10:41:44 -0300

    UI-Test

---
+++ src/main/java/com/TBK/servants_mod/ui/types/type1/Type1Command.java
@@ -0,0 +1,38 @@
+package com.TBK.servants_mod.ui.types.type1;
+
+import com.hypixel.hytale.component.Ref;
+import com.hypixel.hytale.component.Store;
+import com.hypixel.hytale.server.core.command.system.CommandContext;
+import com.hypixel.hytale.server.core.command.system.basecommands.AbstractPlayerCommand;
+import com.hypixel.hytale.server.core.entity.entities.Player;
+import com.hypixel.hytale.server.core.universe.PlayerRef;
+import com.hypixel.hytale.server.core.universe.world.World;
+import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
+
+import javax.annotation.Nonnull;
+
+/**
+ * Command to open the Tutorial Level 1 page.
+ * Usage: /type1
+ */
+public class Type1Command extends AbstractPlayerCommand {
+
+    public Type1Command() {
+        super("type1", "Opens Type 1 - Static Display", false);
+    }
+
+    @Override
+    protected void execute(
+            @Nonnull CommandContext ctx,
+            @Nonnull Store<EntityStore> store,
+            @Nonnull Ref<EntityStore> ref,
+            @Nonnull PlayerRef playerRef,
+            @Nonnull World world
+    ) {
+        Player player = store.getComponent(ref, Player.getComponentType());
+
+        Type1Page page = new Type1Page(playerRef);
+
+        player.getPageManager().openCustomPage(ref, store, page);
+    }
+}
